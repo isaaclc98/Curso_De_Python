5867b84dc8c50f7779a65ed1eda962bcea9d248d
isaaclc98 <37425783+isaaclc98@users.noreply.github.com>
2018-04-07 13:34:30 -0500

    Mi primer programa en python
    Variables, tipos de datos, entradas, salidas, entre otras cosillas

---
+++ miPrimerPrograma.py
@@ -0,0 +1,40 @@
+print ("Hola Como estas?")
+nombre = "YoMeroCaguamero \n";
+print (nombre);
+"""Ejercicio"""
+print("Este es un texto \n");
+nombre=("Isaac")
+edad=20
+print("Hola ",nombre,".Su edad es: ",edad)
+#este es un comentario
+"""Este es un comntario para mas de una linea """
+
+for x in range(10,20):
+    print ("\n",x)
+    
+#Metiendo datos
+print("¿Como se llama?")
+suNombre=input()
+print("Hola",suNombre)
+
+#variables
+nombre=("Isaac")
+edad= 20
+facultad=("Fes Aragon")
+estatura= 1.85
+peso= 97
+carrera= ("Ingenieria en computacion")
+cumpleaños=("22 de enero")
+
+print(nombre,"\n",edad,"\n",facultad,"\n",estatura,"\n",peso,"\n",carrera,"\n",cumpleaños)
+
+#por medio del teclado
+
+nombre=input("Nombre:")
+edad=input("Edad:")
+facultad=input("Facultad")
+estatura=input("Estatrura")
+peso=input("Peso:")
+carrera=input("Carrera:")
+cumpleaños=input("Cumpleaños:")
+print(nombre,"\n",edad,"\n",facultad,"\n",estatura,"\n",peso,"\n",carrera,"\n",cumpleaños)
